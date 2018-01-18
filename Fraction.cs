c8d182ae450d946795ddfe1d159a4ffff83e3041
Daria <islamovaida@yandex.ru>
2018-01-19 01:10:13 +0500

    Initial commit

---
+++ Fraction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFour
+{
+    class Fraction
+    {
+        int numerator;
+        int denominator;
+        
+        public int Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+        public int Denominator
+        {
+            get { return denominator; }
+            set { denominator = value; }
+        }
+
+        public Fraction() // конструктор без параметров
+        {
+            numerator = 0;
+            denominator = 0;
+        }
+        public Fraction(int n, int d) // конструктор с параметрами
+        {
+            this.numerator = n;
+            this.denominator = d;
+        }
+        public void Show() //метод для вывода   
+        {
+            if (denominator != 0)
+            {
+                if (this.denominator > 0) Console.WriteLine("{0} + i{1}", numerator, denominator);
+                else Console.WriteLine("{0} - i{1}", numerator, Math.Abs(denominator));
+            }
+            else Console.WriteLine("{0}", numerator);
+        }
+
+        public override string ToString()
+        {
+            return numerator + "/" + denominator;
+        }
+
+        public static Fraction operator +(Fraction X, Fraction Y)
+        {
+            Fraction res = new Fraction(X.numerator * Y.denominator + X.denominator * Y.numerator, X.denominator + Y.denominator);
+            return res;
+        }
+
+        public static Fraction operator *(Fraction X, Fraction Y)
+        {
+            Fraction res = new Fraction(X.numerator * Y.numerator, X.denominator * Y.denominator);
+            return res;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TaskFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 16;
+            int[] arr = new int[size];
+            FileStream fs = new FileStream("input.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string buf = sr.ReadLine();
+            string[] mas = new string[size];
+            mas = buf.Split(' ');
+            for (int i = 0; i < mas.Length; i++)
+                arr[i] = Convert.ToInt32(mas[i]);
+            sr.Close();
+            fs.Close();
+
+            Fraction x1 = new Fraction(arr[0], arr[1]);
+            Fraction x2 = new Fraction(arr[2], arr[3]);
+            Fraction x3 = new Fraction(arr[4], arr[5]);
+            Fraction x4 = new Fraction(arr[6], arr[7]);
+            Fraction x5 = new Fraction(arr[8], arr[9]);
+            Fraction x6 = new Fraction(arr[10], arr[11]);
+            Fraction x7 = new Fraction(arr[12], arr[13]);
+            Fraction c = new Fraction(arr[14], arr[15]);
+        }
+    }
+}
